485696837815c1ab91ce096f9bc1d7d6391b1cc9
naibabiji <naibabiji@users.noreply.github.com>
2026-05-23 18:28:51 +0800

    修复 logo.png 未嵌入二进制，embed.go 缺少 static/logo.png

--- embed.go
+++ embed.go
@@ -5,7 +5,7 @@ import "embed"
 //go:embed templates/*
 var TemplatesFS embed.FS
 
-//go:embed static/css/* static/js/*
+//go:embed static/css/* static/js/* static/logo.png
 var StaticFS embed.FS
 
 //go:embed wp-panel-cache-helper/*
